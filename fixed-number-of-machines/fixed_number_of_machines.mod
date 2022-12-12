ed6d1d5a5e0682e039508d69a41d6befa518999a
kakasnest <kakasnest@gmail.com>
2022-12-13 00:16:28 +0100

    - JSON output, - Python plot file modified accordingly

--- fixed-number-of-machines/fixed_number_of_machines.mod
+++ fixed-number-of-machines/fixed_number_of_machines.mod
@@ -34,42 +34,20 @@ minimize Makespan:
 
 solve;
 
-printf "import pandas as pd\n";
-printf "import matplotlib.pyplot as plt\n";
-printf "import numpy as np\n";
-printf "from matplotlib.patches import Patch\n";
-printf "from random import randrange\n";
-
-printf "\ndef get_random_color():";
-printf "\n    return '#{:06x}'.format(randrange(256 ** 3))\n";
-
-printf "\nif __name__ == '__main__':";
-printf "\n    vectorize = np.vectorize(int)";
-printf "\n    tasks = [";
-for{t in Tasks}{
-    printf "'%s',",t;
+printf '{';
+printf '"tasks": [';
+for{t in Tasks, m in Machines: assign[t,m] == 1}{
+    printf '{"name":';
+    printf '"%s",',t;
+    printf '"required_time_to_process" : ';
+    printf '%d,',required_time_to_process[t];
+    printf '"start" : ';
+    printf '%d,',start[t];
+    printf '"machine" : ';
+    printf '"%s"',m;
+    printf '}' & (if t == "T"&card(Tasks) then "" else ",");
 }
-printf "]";
-printf "\n    required_time_to_process = [";
-for{t in Tasks}{
-    printf "'%d',",required_time_to_process[t];
-}
-printf "]";
-printf "\n    start = [";
-for{t in Tasks}{
-    printf "'%d',",start[t];
-}
-printf "]";
-printf "\n    colors_for_machines = {";
-for{m in Machines}{
-printf "'%s': get_random_color(),",m;
-}
-printf "}";
-printf "\n    legends = [Patch(facecolor=colors_for_machines[i], label=i) for i in colors_for_machines]";
-printf "\n    fig, ax = plt.subplots(1, figsize=(16,6))";
-printf "\n    ax.barh(tasks,vectorize(required_time_to_process), left=vectorize(start),";
-printf "\n    color=[colors_for_machines[i] for i in colors_for_machines])";
-printf "\n    plt.legend(handles=legends)";
-printf "\n    plt.show()";
+printf ']';
+printf '}';
 
 end;
--- fixed-number-of-machines/gannt.py
+++ fixed-number-of-machines/gannt.py
@@ -1,21 +1,34 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 from matplotlib.patches import Patch
 from random import randrange
+from json import load
 
-def get_random_color():
+def rand_color():
     return '#{:06x}'.format(randrange(256 ** 3))
 
 if __name__ == '__main__':
-    vectorize = np.vectorize(int)
-    tasks = ['T1','T2','T3','T4','T5','T6','T7','T8',]
-    required_time_to_process = ['7','10','9','5','3','1','4','2',]
-    start = ['0','7','0','17','17','22','23','27',]
-    colors_for_machines = {'M0': get_random_color(),'M1': get_random_color(),'M2': get_random_color(),}
-    legends = [Patch(facecolor=colors_for_machines[i], label=i) for i in colors_for_machines]
-    fig, ax = plt.subplots(1, figsize=(16,6))
-    ax.barh(tasks,vectorize(required_time_to_process), left=vectorize(start),
-    color=[colors_for_machines[i] for i in colors_for_machines])
-    plt.legend(handles=legends)
-    plt.show()
+    with open("result.json") as json_data:
+        data = load(json_data)
+        # vectorize = np.vectorize(int)
+        colors_for_machines = dict()
+        machines = set()
+        for task in data["tasks"]:
+            if task["machine"] in machines:
+                task = {**task, "color": colors_for_machines[task["machine"]]}
+            else:
+                color = rand_color()
+                task = {**task, "color": color}
+                machines.add(task["machine"])
+                colors_for_machines[task["machine"]] = color
+
+        task_list = [task["name"] for task in data["tasks"]]
+        legends = [Patch(facecolor=colors_for_machines[i], label=i) for i in colors_for_machines]
+        req_time_list = [task["required_time_to_process"] for task in data["tasks"]]
+        start_list = [task["start"] for task in data["tasks"]]
+        color_list = [colors_for_machines[i] for i in colors_for_machines]
+
+        fig, ax = plt.subplots(1, figsize=(16,6))
+        ax.barh(task_list,req_time_list, left=start_list,
+        color= color_list)
+        plt.legend(handles=legends)
+        plt.show()
